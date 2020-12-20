17ac21de00fa3e4da8c9ae42c5136da667cfdb71
Allan Regush <17693494+AllanRegush@users.noreply.github.com>
2020-12-19 23:52:13 -0600

    Move Static Classes

---
+++ Object Oriented Programming/StaticClasses/ConsoleUI/CalculateData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public static class CalculateData
+    {
+        public static double Add(double x, double y)
+        {
+            return x + y;
+        }
+
+        public static double Subtract(double x, double y)
+        {
+            return x - y;
+        }
+
+        public static double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+
+        public static double Divide(double x, double y)
+        {
+            try
+            {
+                double output = x / y;
+                return output;
+            }
+            catch(Exception ex)
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ Object Oriented Programming/StaticClasses/ConsoleUI/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double addResult = CalculateData.Add(2, 3);
+            double subResult = CalculateData.Subtract(1, 1);
+            double multResult = CalculateData.Multiply(1, 2);
+            double divideResult = CalculateData.Divide(1, 1);
+            Console.WriteLine($"Add Result: {addResult}, Subtract Result: {subResult}");
+            Console.WriteLine($"Multiply Result: {multResult}, DivideResult: {divideResult}");
+            Console.ReadLine();
+        }
+    }
+}
