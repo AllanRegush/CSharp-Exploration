8df734a3b8710a4faa1305599ec0938cac21cd24
Allan Regush <17693494+AllanRegush@users.noreply.github.com>
2021-12-17 01:23:14 -0600

    Project Types: (Shopping List) Added List of items
    This now allows a list of items to be displayed :^)

---
+++ Project Types/ShoppingListApp/ShoppingList/ShoppingList/Models/ShoppingItemModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingList.Models
+{
+    internal class ShoppingItemModel
+    {
+        public int Id { get; set; }
+        public string Item { get; set; }
+        public bool IsCrossedOff { get; set; }
+    }
+}
--- Project Types/ShoppingListApp/ShoppingList/ShoppingList/Views/ListPage.xaml.cs
+++ Project Types/ShoppingListApp/ShoppingList/ShoppingList/Views/ListPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using ShoppingList.Models;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -8,34 +10,82 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+
 namespace ShoppingList.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ListPage : ContentPage
     {
         private readonly string  _fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "notes.txt");
+        
+        private  List<ShoppingItemModel> _shoppingItems;
+
         public ListPage()
         {
             InitializeComponent();
-
+            //_shoppingItems = new List<ShoppingItemModel>();
+            
+            
             if (File.Exists(_fileName))
             {
-                editor.Text = File.ReadAllText(_fileName);
+                string shoppingItemsJson = File.ReadAllText(_fileName);
+                try
+                {
+                    _shoppingItems = JsonConvert.DeserializeObject<List<ShoppingItemModel>>(shoppingItemsJson);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    _shoppingItems = new List<ShoppingItemModel>();
+                }
+            } 
+            else
+            {
+                _shoppingItems = new List<ShoppingItemModel>();
+            }
+            
+            
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            try
+            {
+                collectionView.ItemsSource = _shoppingItems
+                    .OrderBy(item => item.IsCrossedOff)
+                    .ToList();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
             }
         }
 
         void OnSaveButtonClicked(object sender, EventArgs e)
         {
-            File.WriteAllText(_fileName, editor.Text);
+            ShoppingItemModel shoppingItem = new ShoppingItemModel { Id = _shoppingItems.Count + 1, Item = editor.Text, IsCrossedOff = false };
+            _shoppingItems.Add(shoppingItem);
+            collectionView.ItemsSource = _shoppingItems
+            .OrderBy(item => item.IsCrossedOff)
+            .ToList();
+            var json = JsonConvert.SerializeObject(_shoppingItems);
+            File.WriteAllText(_fileName, json);
         }
 
-        void OnDeleteButtonClicked(object sendor, EventArgs e)
+        void OnDeleteButtonClicked(object sender, EventArgs e)
         {
             if (File.Exists(_fileName))
             {
                 File.Delete(_fileName);
             }
             editor.Text = "";
+        }
+
+        void OnSelectionChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
